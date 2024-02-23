91e383082e8619adddd05928f11a43517bc2b64a
SeifEleslam <seifgouda98@gmail.com>
2024-02-23 14:56:04 +0200

    fixed

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -2,8 +2,8 @@
 file { '/tmp/school':
   ensure  => file,
   path    => '/tmp/school',
-  content => 'I love puppet',
+  mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  mode    => '0744',
+  content => 'I love puppet',
 }
