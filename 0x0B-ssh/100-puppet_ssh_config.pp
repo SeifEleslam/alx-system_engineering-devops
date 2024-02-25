a9482acd134b63762c29afcc24e2c7a57ead48ea
SeifEleslam <seifgouda98@gmail.com>
2024-02-25 20:52:36 +0200

    task100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,9 @@
 # Edit ssh configuration
-file_line { '/etc/ssh/sshd_config':
+file_line { '/etc/ssh/ssh_config':
   ensure => present,
   line   => 'IdentityFile ~/.ssh/id_rsa',
 }
-file_line { '/etc/ssh/sshd_config':
+file_line { '/etc/ssh/ssh_config':
   ensure => present,
   line   => 'PasswordAuthentication no',
 }
