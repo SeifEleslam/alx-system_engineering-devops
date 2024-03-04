e8afe27221b6426eb7fd70017a55d97499c921f3
SeifEleslam <seifgouda98@gmail.com>
2024-03-04 23:21:37 +0200

    task2

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,42 @@
+# Install nginx wit puppet
+package { 'nginx' :
+  ensure   => latest,
+}
+# Configure the server block for our site.
+file { '/var/www/html':
+  ensure => directory,
+  owner  => root,
+  group  => root,
+  mode   => '0755',
+}
+# Create a simple index.html file to test the
+file { '/var/www/html/index.html':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  content => 'Hello World!',
+}
+# Configure server
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  owner   => root,
+  group   => root,
+  mode    => '0644',
+  content => '
+server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    root /var/www/html;
+    index index.html index.htm index.nginx-debian.html;
+    location / {
+        add_header X-Served-By $HOSTNAME;
+    }
+}
+',
+}
+# Restart Service
+service { 'nginx':
+  ensure => running,
+  enable => true,
+}
