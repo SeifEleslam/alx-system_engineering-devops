64cb375bb16564ab849d25da974525332611344b
SeifEleslam <seifgouda98@gmail.com>
2024-03-04 23:54:57 +0200

    task2

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,46 +1,19 @@
 # Install nginx with puppet
-exec { 'update':
-  command  => 'sudo apt-get update',
-  provider => shell,
-}
 package { 'nginx' :
-  ensure   => present,
+  ensure   => latest,
 }
-# Configure the server block for our site.
-file { '/var/www/html':
-  ensure => directory,
-  owner  => root,
-  group  => root,
-  mode   => '0755',
-}
-# Create a simple index.html file to test the
-file { '/var/www/html/index.html':
-  ensure  => file,
-  owner   => root,
-  group   => root,
-  mode    => '0644',
-  content => 'Hello World!',
-}
+
 # Configure server
-file { 'config nginx':
-  ensure  => file,
-  path    => '/etc/nginx/sites-enabled/default',
-  owner   => root,
-  group   => root,
-  mode    => '0644',
-  content => "
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
-    root /var/www/html;
-    index index.html index.htm index.nginx-debian.html;
-    location / {
-      add_header X-Served-By ${hostname};
-    }}
-",
+file_line { 'header line':
+  ensure => present,
+  path   => '/etc/nginx/sites-available/default',
+  line   => "	location / {
+  add_header X-Served-By ${hostname};",
+  match  => '^\tlocation / {',
 }
+
 # Restart Service
-exec { 'restart_nginx':
-  command  => 'sudo service nginx restart',
-  provider => shell,
+service { 'nginx':
+  ensure => running,
+  enable => true,
 }
