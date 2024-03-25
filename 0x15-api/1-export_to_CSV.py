7726df65935c48c9d9a76364f65479100cbaffb5
SeifEleslam <seifgouda98@gmail.com>
2024-03-25 23:41:35 +0200

    task1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" 0. Gather data from an API """
+from csv import writer
+import requests
+from sys import argv as args
+
+url = "https://jsonplaceholder.typicode.com/"
+
+
+def request_data(endpoint, method, params):
+    """Requesting method"""
+    return requests.request(method, url+endpoint, params=params)
+
+
+if __name__ == "__main__":
+    """ Work on import only """
+    data = list()
+    user = request_data(f"users/{args[1]}", "GET", None).json()
+    todos = request_data("todos", "GET", {'userId': args[1]}).json()
+    for todo in todos:
+        data.append([todo["userId"],
+                     user["username"],
+                     todo["completed"],
+                     todo["title"]])
+    with open("data.csv", "w", newline="") as f:
+        writer = writer(f)
+        writer.writerows(data)
