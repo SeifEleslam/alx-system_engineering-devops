5dd771b0347423049a82d724c909196ced8b3fa4
SeifEleslam <seifgouda98@gmail.com>
2024-04-09 14:24:26 +0200

    task1

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""
+Reddit API hot posts
+"""
+
+from requests import get
+
+
+def top_ten(subreddit):
+    """Get Titles of top 10 hot posts"""
+
+    subreddit_info = get(
+        "https://www.reddit.com/r/{:s}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "Custom-User-Agent"},
+        allow_redirects=False)
+
+    if subreddit_info.status_code >= 300:
+        print(None)
+    else:
+        for post in subreddit_info.json().get('data').get('children'):
+            print(post.get('data').get('title'))
