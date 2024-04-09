e0dfcaf1c82bc7f905ff5e64d5620f6af83f3850
SeifEleslam <seifgouda98@gmail.com>
2024-04-09 13:35:46 +0200

    task0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+# Reddit API subscribers
+import requests
+
+
+def number_of_subscribers(subreddit):
+    subreddit_info = requests.get(
+        f"https://www.reddit.com/r/{subreddit}/about.json").json()
+    subreddit_data = subreddit_info.get('data')
+    print(subreddit_data.get('subscribers')
+          if subreddit_data and subreddit_data.get('subscribers') else 0)
+
+
+if __name__ == "__main__":
+    number_of_subscribers("learnpython")
