db20dda7368882dd728bba9093988134c8f94961
SeifEleslam <seifgouda98@gmail.com>
2024-04-09 15:28:32 +0200

    task2

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -6,16 +6,23 @@ Reddit API hot posts
 from requests import get
 
 
-def recurse(subreddit, hot_list=[]):
+def recurse(subreddit, hot_list=[], after=''):
     """Get Titles of top 10 hot posts"""
 
     subreddit_info = get(
-        "https://www.reddit.com/r/{:s}/hot.json?limit=10".format(subreddit),
+        "https://www.reddit.com/r/{:s}/hot.json?limit=100&after={}"
+        .format(subreddit, after),
         headers={"User-Agent": "Custom-User-Agent"},
         allow_redirects=False)
 
     if subreddit_info.status_code >= 300:
-        print(None)
+        return (None)
     else:
-        for post in subreddit_info.json().get('data').get('children'):
-            print(post.get('data').get('title'))
+        posts = subreddit_info.json().get('data').get('children')
+        li = [post.get('data').get('title')
+              for post in posts]
+        if len(li) == 0:
+            return (hot_list)
+        hot_list.extend(li)
+        return recurse(subreddit, hot_list,
+                       "{}".format(posts[-1].get('data').get('name')))
