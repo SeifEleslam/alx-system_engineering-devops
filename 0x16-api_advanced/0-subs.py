f4d29de49d34bed629a447e0504d20db7a6a112c
SeifEleslam <seifgouda98@gmail.com>
2024-04-09 13:46:24 +0200

    task0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -9,6 +9,7 @@ def number_of_subscribers(subreddit):
     """Get Number of subscribers"""
     subreddit_info = requests.get(
         "https://www.reddit.com/r/{}/about.json".format(subreddit),
+        headers={"User-Agent": "Custom-User-Agent"},
         allow_redirects=False)
     return 0 if subreddit_info.status_code >= 300 else \
         subreddit_info.json().get('data').get('subscribers')
