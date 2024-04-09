bbe4d4f1270ce60c41e1f982cb87f3042aad89d6
SeifEleslam <seifgouda98@gmail.com>
2024-04-09 14:27:21 +0200

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,27 +1,21 @@
 #!/usr/bin/python3
 """
-queries the Reddit API and prints the titles of
-the first 10 hot posts listed for a given subreddit.
+Reddit API hot posts
 """
-import requests
+
+from requests import get
 
 
 def top_ten(subreddit):
-    """
-    prints the titles of the first 10 hot posts listed for
-    a given subreddit
-    """
-    url = ("https://api.reddit.com/r/{}?sort=hot&limit=10".format(subreddit))
-    headers = {'User-Agent': 'CustomClient/1.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
+    """Get Titles of top 10 hot posts"""
 
-    if response.status_code != 200:
+    subreddit_info = get(
+        "https://www.reddit.com/r/{:s}/hot.json?limit=10".format(subreddit),
+        headers={"User-Agent": "Custom-User-Agent"},
+        allow_redirects=False)
+
+    if subreddit_info.status_code >= 300:
         print(None)
-        return
-    response = response.json()
-    if 'data' in response:
-        for posts in response.get('data').get('children'):
-            print(posts.get('data').get('title'))
-
     else:
-        print(None)
+        for post in subreddit_info.json().get('data').get('children'):
+            print(post.get('data').get('title'))
